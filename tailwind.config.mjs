33d16d3766fe6bf34fab3524a793ab765fb1ff92
Ares <diego.amachiflores@gmail.com>
2024-06-06 17:01:30 -0500

    06/06/2024

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily:{
+			'title':['Pixeled'],
+			'body':['Pixelify Sans Variable']
+		}
 	},
 	plugins: [],
 }
